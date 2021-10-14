483c5000921456ae293930c05e02f24085fc62ab
lucky-786 <17bcs1240@cuchd.in>
2021-10-14 11:42:41 +0530

    Created 1st web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) { //Function that handle a particular web request
+	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+}
 
 func main() {
-	fmt.Println("Welcome to golang")
+	http.HandleFunc("/", handlerFunc) //Function giving path and calling handlerFunc
+	http.ListenAndServe(":8080", nil) //Start the Web Server
 }
